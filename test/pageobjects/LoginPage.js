8addbf6b7402ed77fbc9644b498f31f4a3577b9e
Seu Nome <seuemail@exemplo.com>
2025-11-29 08:48:14 -0300

    teste login

--- test/pageobjects/LoginPage.js
+++ test/pageobjects/LoginPage.js
@@ -1,16 +1,18 @@
 // test/pageobjects/LoginPage.js
 class LoginPage {
-    get inputUsername() { return $('~username-input'); }
-    get inputPassword() { return $('~password-input'); }
+    get inputEmail() { return $('//*[@text="Email"]'); }
+    get inputPassword() {  return $('//*[@text="Password"]'); }
     get btnLogin() { return $('~login-btn'); }
     get welcomeText() { return $('~welcome-message'); }
+    get successMessage() { return $('//*[@text="You are logged in!"]'); }
+    get loginText() { return $('//*[@text="Login"]'); }
 
     async login(username = 'bob@example.com', password = '10203040') {
-        await this.inputUsername.setValue(username);
+        await this.loginText.click();
+        await this.inputEmail.setValue(username);
         await this.inputPassword.setValue(password);
         await this.btnLogin.click();
-        await expect(this.welcomeText).toBeExisting();
-        await expect(this.welcomeText).toHaveTextContaining('Welcome');
+        await expect(this.successMessage).toBeExisting();
     }
 }
 
