b533faf29b4a8e1dcb34c68d75d230a8de5cb3ad
Seu Nome <seuemail@exemplo.com>
2025-12-17 09:29:37 -0300

    correção xpath

--- test/pageobjects/FormPage.js
+++ test/pageobjects/FormPage.js
@@ -11,6 +11,7 @@ class FormPage {
         await this.formsText.click();
         await this.typeText.click();
         await this.typeText.setValue(textExample);
+        await this.hideKeyboard();
         await this.activeText.click();
         await this.activeMessageText.waitForDisplayed({ timeout: 50000 });
 
